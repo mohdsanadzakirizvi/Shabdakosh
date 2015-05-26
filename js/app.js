7fd29c1064cfe8909a8ec3c64251b4289b5de7ed
theuchiha <mohdsanadzakirizvi@users.noreply.github.com>
2015-05-26 22:56:11 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,62 +1,29 @@
-(function(){
-	var app=angular.module('game',[]);
+(function() {
+    var app = angular.module('game', []);
 
 
-	app.controller('formCtrl',function($scope){
-  	$scope.isVisible=false;
- 
-		$scope.x=-1;
-		$scope.translations=[];
-		$scope.words=["Radio","Train","Welcome!","table"];
-        $scope.image=["images/radio.png","images/train.jpg","images/welcome!.jpg","images/table.jpg"];
+    app.controller('formCtrl', function($scope) {
+        $scope.isVisible = false;
 
+        $scope.x = -1;
+        $scope.translations = [];
+        $scope.words = ["Radio", "Train", "Welcome!", "table"];
+        $scope.image = ["images/radio.png", "images/train.jpg", "images/welcome!.jpg", "images/table.jpg"];
+        $scope.submit = function() {
+            if ($scope.x < $scope.words.length) {
+                $scope.x++;
+                if ($scope.input)
 
-           $scope.submit=function(){
-              
+                    $scope.translations.push($scope.input);
+                $scope.input = "";
+            }
+        }
 
-           	if(	$scope.x<$scope.words.length){
-           	$scope.x++;
-            if($scope.input)
-         
-            $scope.translations.push($scope.input); 
-            $scope.input="";
-          
+        $scope.onItemClicked = function(item) {
 
- 
-
-
-         }
-          
-
-         
-      }
-      
-       $scope.onItemClicked=function(item){
-        
-       	$scope.isVisible=true;
-       }
-          
-
-
-
-
-
-
-
-	});
-       
-app.controller('tableCtrl',function($scope){
-  
-  
-});
-
-
-
-
-
-
-
-
-
+            $scope.isVisible = true;
+        }
+    });
+    app.controller('tableCtrl', function($scope) {});
 
 })();
